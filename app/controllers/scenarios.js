922f76614ec335dfc8a6965e0a69c0150574b653
Miguel Camba <miguel.camba@gmail.com>
2014-08-15 16:51:56 +0100

    Allow to customize the host of the adapter.

--- app/controllers/scenarios.js
+++ app/controllers/scenarios.js
@@ -4,9 +4,9 @@ export default Ember.ArrayController.extend({
   environment: null,
   environments: [
     { id: 'uat', value: 'http://localhost:5000' },
-    { id: 'dev', value: 'http://localhost:5000' },
-    { id: 'si', value: 'http://localhost:5000' },
-    { id: 'test', value: 'http://localhost:5000' }
+    { id: 'dev', value: 'http://localhost:4000' },
+    { id: 'si', value: 'http://localhost:3000' },
+    { id: 'test', value: 'http://localhost:2000' }
   ],
 
   // Observers
@@ -18,5 +18,12 @@ export default Ember.ArrayController.extend({
     } else {
       this.transitionToRoute("scenarios");
     }
-  }.observes('content.selected')
+  }.observes('content.selected'),
+
+  environmentObserver: function(){
+    var id = this.get('environment');
+    var environment = this.get('environments').find(function(e){ return e.id == id });
+    Cyclopta.__container__.lookup('adapter:application').set('host', environment.value);
+  }.observes('environment'),
+
 });
