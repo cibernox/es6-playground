671e50e2bc5c76340799f2382d1c950630f9cfe9
Miguel Camba <miguel.camba@gmail.com>
2014-08-19 23:37:24 +0100

    Fixed some deprecation warnings

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  // namespace: 'api',
-  host: 'http://localhost:5000',
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
+  namespace: 'api',
+  // host: 'http://localhost:5000',
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*'
+  // }
 });
